cb354276630ed1aea54e523668af01108cc9199e
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-12-02 14:23:48 +0530

    Update Day16_Anagram.cpp

--- String/Day16_Anagram.cpp
+++ String/Day16_Anagram.cpp
@@ -1,20 +1,57 @@
-/*
-    Approach - 1 Using Unordered_map (Hashtable)
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
 class Solution {
   public:
     // Function is to check whether two strings are anagram of each other or not.
     bool areAnagrams(string& s1, string& s2) {
-        // Your code here
-        unordered_map<char,int> map;
-        if(s1.size() != s2.size()) return 0;
-        for(char ch:s1) map[ch]++;
-        for(char ch:s2){
-            if(map[ch]) map[ch]--;
-            if(map[ch] == 0)  map.erase(ch);
+         // If the lengths are different, they cannot be anagrams
+        if (s1.size() != s2.size()) {
+            return false;
         }
-        return map.size()==0?1:0;
+
+        // Create an array to store frequency of characters
+        int freq[26] = {0}; // Array to count frequency of characters
+
+        // Traverse both strings and update the frequency array
+        for (int i = 0; i < s1.size(); i++) {
+            freq[s1[i] - 'a']++; // Increment frequency for s1
+            freq[s2[i] - 'a']--; // Decrement frequency for s2
+        }
+
+        // If all values in frequency array are 0, then they are anagrams
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
