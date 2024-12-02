a47c4730f7549fc5d249e8c85c56537517a8e9f2
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-12-02 14:16:52 +0530

    Update Day1_Second_Largest.cpp

--- Arrays/Day1_Second_Largest.cpp
+++ Arrays/Day1_Second_Largest.cpp
@@ -1,16 +1,55 @@
-// User function template for C++
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
 class Solution {
   public:
-    // Function returns the second
-    // largest elements
-//  Time Complexity: O(n) | Space Complexity: O(1)
+    // Function returns the second largest element
     int getSecondLargest(vector<int> &arr) {
-        // Code Here
-        int maxa = -1 , maxa2 =  -1;
-        for(int i = 0  ; i<arr.size();i++){
-            if(maxa < arr[i]){ maxa2 = maxa ; maxa = arr[i];}
-            else if(maxa2 < arr[i] && arr[i] != maxa) maxa2 = arr[i];
+        int largest = INT_MIN;  // Variable to store the largest element
+        int secondLargest = INT_MIN;  // Variable to store the second largest element
+
+        for (int num : arr) {
+            if (num > largest) {
+                // Update second largest and largest
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                // Update second largest if a valid candidate is found
+                secondLargest = num;
+            }
         }
-        return maxa2;
+
+        // If no valid second largest element exists, return -1
+        return (secondLargest == INT_MIN) ? -1 : secondLargest;
     }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
