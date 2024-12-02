288a9330b9948d6848725955441aedce0c88dbd7
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-12-02 14:24:39 +0530

    Update Day17_Non_Repeating_Character.cpp

--- String/Day17_Non_Repeating_Character.cpp
+++ String/Day17_Non_Repeating_Character.cpp
@@ -1,20 +1,60 @@
-/*
-    Approach - 1 Using Unordered_map (Hashtable / Hashing / Hash)
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
 class Solution {
   public:
     // Function to find the first non-repeating character in a string.
     char nonRepeatingChar(string &s) {
-        // Your code here
-        // Time Complexity: O(n)
-        // Space Complexity: O(26)
-        unordered_map<char,int> map;
-        for(char ch:s) map[ch]++;
-        for(char ch:s){
-            if(map[ch]==1) return ch;
+      int freq[26] = {0}; // Frequency array for lowercase Latin letters
+
+        // Count the frequency of each character in the string
+        for (char c : s) {
+            freq[c - 'a']++;
         }
+
+        // Traverse the string again to find the first non-repeating character
+        for (char c : s) {
+            if (freq[c - 'a'] == 1) {
+                return c;  // Return the first non-repeating character
+            }
+        }
+
+        // If no non-repeating character is found, return '$'
         return '$';
     }
 };
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
