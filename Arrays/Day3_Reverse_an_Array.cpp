51a4cb319a763f5aa36f7c2f95a08c32157954f6
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-12-02 14:18:10 +0530

    Update Day3_Reverse_an_Array.cpp

--- Arrays/Day3_Reverse_an_Array.cpp
+++ Arrays/Day3_Reverse_an_Array.cpp
@@ -1,25 +1,45 @@
-/*
-    Approach-1 (without Using In-Built Function)
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
 class Solution {
   public:
     void reverseArray(vector<int> &arr) {
-        // code here
-        reverse(arr.begin(),arr.end());
-    }
-};
-/*
-    Approach-2 (Using In-Built Function)
-    Time Complexity O(n)
-    Space Complexity O(1)
-*/
-class Solution {
-  public:
-    void reverseArray(vector<int> &arr) {
-        // code here
-        reverse(arr.begin(),arr.end());
+        // Reversing the array in place using two-pointer technique
+        int left = 0, right = arr.size() - 1;
+        while (left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
     }
 };
 
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
