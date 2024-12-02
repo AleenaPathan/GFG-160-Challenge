7052f67c1542b25b3067b67bd3d08558826e4e8e
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-12-02 14:20:03 +0530

    Update Day7_Stock_Buy_and_Sell_Multiple_Transaction_Allowed.cpp

--- Arrays/Day7_Stock_Buy_and_Sell_Multiple_Transaction_Allowed.cpp
+++ Arrays/Day7_Stock_Buy_and_Sell_Multiple_Transaction_Allowed.cpp
@@ -1,46 +1,50 @@
-/*
-    Approach - 1 (Using Accumulate Profit)
-    Time Complexity O(n)
-    Space Complexity O(1)
-*/
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
 
 class Solution {
   public:
     int maximumProfit(vector<int> &prices) {
-        // code here
-        // Accumate Profit
-     // Time Complexity O(n) | Space Complexity O(1);
-     int maxProfit = 0  ;
-     for(int i=1;i<prices.size();i++){
-         if(prices[i] > prices[i-1]) maxProfit += prices[i] - prices[i-1];
-     }
-     return maxProfit;
-    }
-};
-/*
-    Approach - 2 (Using Local Minima and Maxima)
-    Time Complexity O(n)
-    Space Complexity O(1)
-*/
-class Solution {
-  public:
-    int maximumProfit(vector<int> &prices) {
-        // code here
-    // Local Maxima and Minima
-    // Time Complexity: O(n) | Space Complexity : O(1)
-        int maxa = prices[0] , n =prices.size();
-        int mina = prices[0];
+        int n = prices.size();
         int maxProfit = 0;
-        int i =0; // track our stock day;
-        while(i<n-1){
-            while(i<n-1 && prices[i]>=prices[i+1]){i++;}
-            mina = prices[i];
-            
-            while(i<n-1 && prices[i] <= prices[i+1]){i++;}
-            maxa = prices[i];
-            
-            maxProfit += (maxa - mina);
+
+        // Iterate through the array and accumulate profits for every rise
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
         }
+
         return maxProfit;
     }
 };
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        int res = ob.maximumProfit(arr);
+        cout << res;
+
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
